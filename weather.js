f78394c012ac82d9255cf6e0a3d836aa2732879b
parkcor <59765213+parkcor@users.noreply.github.com>
2020-01-22 19:43:45 +0900

    2nd commit

--- weather.js
+++ weather.js
@@ -12,7 +12,7 @@ function getWeather(lat, lng) {
         .then(function (json) {
             const temperature = json.main.temp;
             const place = json.name;
-            weather.innerText = `${temperature} @ ${place}`;
+            weather.innerText = `현재기온 : ${temperature}°C @ ${place}`;
         });
 }
 
